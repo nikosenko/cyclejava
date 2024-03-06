3d147467900913d636fb4f1a54018e99bc52f4d9
Natalia test <n.i.kosenko@bk.ru>
2024-03-06 13:44:07 +0300

    Parameterized test

--- src/test/java/ru/netology/mvnjava/services/CalcServiceTest.java
+++ src/test/java/ru/netology/mvnjava/services/CalcServiceTest.java
@@ -2,25 +2,21 @@ package ru.netology.mvnjava.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class CalcServiceTest {
 
 
-    @Test
-    public void CalcServiceMonthsOfRest10() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/month.csv")
+    public void CalcServiceMonthsOfRest(int expected, int income, int expenses, int threshold) {
         CalcService service = new CalcService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void CalcServiceMonthsOfRest100() {
-        CalcService service = new CalcService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
 }
 
