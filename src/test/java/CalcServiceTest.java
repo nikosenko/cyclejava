b167d30708c79bd2b11b9df378b408a1b053d207
Natalia test <n.i.kosenko@bk.ru>
2024-03-06 10:22:05 +0300

    Packages and cycles

---
+++ src/test/java/CalcServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.mvnjava.services.CalcService;
+
+public class CalcServiceTest {
+
+
+    @Test
+    public void CalcServiceMonthsOfRest10() {
+        CalcService service = new CalcService();
+        int expected = 3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalcServiceMonthsOfRest100() {
+        CalcService service = new CalcService();
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        Assertions.assertEquals(expected, actual);
+    }
+}
